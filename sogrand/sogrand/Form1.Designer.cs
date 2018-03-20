1aa1725aa7984306edeeb42384ede0a3c154375e
souluses <37599131+souluses@users.noreply.github.com>
2018-03-20 23:48:55 +0200

    Initial commit

---
+++ sogrand/sogrand/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace sogrand
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.выводToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.выходToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.информацияToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cmbbx = new System.Windows.Forms.ComboBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(293, 265);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(349, 237);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.Indigo;
+            this.label1.Location = new System.Drawing.Point(84, 112);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Выберите вершину:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.Transparent;
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.выводToolStripMenuItem,
+            this.выходToolStripMenuItem,
+            this.информацияToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(654, 28);
+            this.menuStrip1.TabIndex = 2;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // выводToolStripMenuItem
+            // 
+            this.выводToolStripMenuItem.ForeColor = System.Drawing.Color.Indigo;
+            this.выводToolStripMenuItem.Name = "выводToolStripMenuItem";
+            this.выводToolStripMenuItem.Size = new System.Drawing.Size(66, 24);
+            this.выводToolStripMenuItem.Text = "Вывод";
+            this.выводToolStripMenuItem.Click += new System.EventHandler(this.выводToolStripMenuItem_Click);
+            // 
+            // выходToolStripMenuItem
+            // 
+            this.выходToolStripMenuItem.ForeColor = System.Drawing.Color.Indigo;
+            this.выходToolStripMenuItem.Name = "выходToolStripMenuItem";
+            this.выходToolStripMenuItem.Size = new System.Drawing.Size(65, 24);
+            this.выходToolStripMenuItem.Text = "Выход";
+            this.выходToolStripMenuItem.Click += new System.EventHandler(this.выходToolStripMenuItem_Click);
+            // 
+            // информацияToolStripMenuItem
+            // 
+            this.информацияToolStripMenuItem.ForeColor = System.Drawing.Color.Indigo;
+            this.информацияToolStripMenuItem.Name = "информацияToolStripMenuItem";
+            this.информацияToolStripMenuItem.Size = new System.Drawing.Size(114, 24);
+            this.информацияToolStripMenuItem.Text = "Информация";
+            this.информацияToolStripMenuItem.Click += new System.EventHandler(this.информацияToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::sogrand.Properties.Resources.pic1;
+            this.pictureBox1.Location = new System.Drawing.Point(300, 41);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(330, 203);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // cmbbx
+            // 
+            this.cmbbx.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbbx.FormattingEnabled = true;
+            this.cmbbx.Items.AddRange(new object[] {
+            "A",
+            "B",
+            "C",
+            "D",
+            "E",
+            "F",
+            "G"});
+            this.cmbbx.Location = new System.Drawing.Point(90, 132);
+            this.cmbbx.Name = "cmbbx";
+            this.cmbbx.Size = new System.Drawing.Size(121, 24);
+            this.cmbbx.TabIndex = 4;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 265);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(274, 237);
+            this.textBox1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::sogrand.Properties.Resources.bcg;
+            this.ClientSize = new System.Drawing.Size(654, 514);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cmbbx);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Дискретные структуры";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem выводToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem выходToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem информацияToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ComboBox cmbbx;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ sogrand/sogrand/Form1.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace sogrand
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string s;
+        int[,] arr = new int[7, 7];
+        double[,] B = new double[7, 7];
+        //double Det;
+
+        private void выходToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void информацияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Бударова А.С., группа 622п.", "Информация",
+                MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void выводToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int Num = (cmbbx.SelectedIndex+1);
+                if (Num == 0) throw new Exception("Начальная вершина не выбрана!");
+
+                arr[0, 2] = 1; arr[0, 3] = 1; arr[0, 5] = 1;
+                arr[1, 0] = 1; arr[1, 4] = 1; arr[1, 6] = 1;
+                arr[2, 1] = 1; arr[2, 5] = 1;
+                arr[3, 0] = 1; arr[3, 1] = 1;
+                arr[4, 2] = 1; arr[4, 6] = 1;
+                arr[5, 0] = 1; arr[5, 4] = 1;
+                arr[6, 1] = 1; arr[6, 3] = 1;
+                Output_Matrix(arr, dataGridView1);
+                countD(arr, Num);
+                var Det = DetRec(B);
+                textBox1.Text = Det.ToString();
+            }
+            catch (Exception ex)
+            { MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+
+        }
+
+        public void Output_Matrix(int[,] Mas, DataGridView dtgrdvw)
+        {
+            char nextChar = 'A';
+            dtgrdvw.ColumnCount = 0;
+            dtgrdvw.ColumnCount = 7;
+            dtgrdvw.Rows.Add(7);
+            dtgrdvw.RowCount =7;
+            dtgrdvw.ColumnCount = 7;
+            for (int i = 0; i < 7; ++i)
+                for (int j = 0; j < 7; ++j)
+                {
+                    dtgrdvw.Rows[i].Cells[j].Value = Mas[i, j];
+                }
+
+            for (int i = 0; i < 7; i++)
+
+            {
+                dtgrdvw.Columns[i].ReadOnly = true;
+                dtgrdvw.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+                dtgrdvw.Columns[i].Name = i.ToString();
+                dtgrdvw.Columns[i].Width = 20;
+                dtgrdvw.RowHeadersWidth = 50;
+                dtgrdvw.Columns[i].Width = 30;
+                dtgrdvw.Columns[i].HeaderText = nextChar.ToString();
+                dtgrdvw.Rows[i].HeaderCell.Value = nextChar.ToString();
+                nextChar = (char)(((int)nextChar) + 1);
+
+            }
+        }
+
+        public void countD(int[,] Mas, int Num)
+        {
+            int[,] D = new int[7, 7];
+            for (int i = 0; i < 7; i++)
+                for (int j = 0; j < 7; j++)
+                {
+                    if (arr[j, i] == 1)
+                        D[i, i]++;
+                }
+
+            for (int i = 0; i < 7; i++)
+                for (int j = 0; j < 7; j++)
+                {
+                    B[i, j] = D[i, j] - Mas[i, j];
+                }
+
+            //int[,] F = new int[7, 7];
+            //for (int i = 0; i < 8; i++)
+            //    for (int j = 0; j < 8; j++)
+            //    {
+            //        if (j < Num)
+            //        {
+            //            F[i, j] = B[i, j];
+            //            F[j, i] = B[j, i];
+            //        }
+            //        else
+            //        {
+            //            F[i, j] = B[i, j + 1];
+            //            F[j, i] = B[j + 1, i];
+            //        }
+            //    }
+        }
+
+        private static double DetRec(double[,] matrix)
+        {
+            if (matrix.Length == 4)
+            {
+                return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            }
+            double sign = 1, result = 0;
+            for (int i = 0; i < matrix.GetLength(1); i++)
+            {
+                double[,] minor = GetMinor(matrix, i);
+                result += sign * matrix[0, i] * DetRec(minor);
+                sign = -sign;
+            }
+            return result;
+        }
+
+        private static double[,] GetMinor(double[,] matrix, int n)
+        {
+            double[,] result = new double[matrix.GetLength(0) - 1, matrix.GetLength(0) - 1];
+            for (int i = 1; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0, col = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (j == n)
+                        continue;
+                    result[i - 1, col] = matrix[i, j];
+                    col++;
+                }
+            }
+            return result;
+        }
+    }
+}
